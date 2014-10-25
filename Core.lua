b30cb24a0e074ef36cbdb4972a4f0da57c120088
Nico Weichbrodt <nico@weichbrodt.me>
2014-10-25 21:14:23 +0200

    First commit

---
+++ Core.lua
@@ -0,0 +1,14 @@
+Addon = LibStub("AceAddon-3.0"):NewAddon("BalanceSpellSuggest")
+
+function Addon:OnInitialize()
+    print("BalanceSpellSuggest")
+end
+
+function Addon:OnEnable()
+    -- setup frame
+    print("BSP enabled")
+end
+
+function Addon:OnDisable()
+    print("BSP disabled")
+end
